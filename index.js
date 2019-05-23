05840e810a07d97ab329489858089a5cf2499301
Christian Rodriguez <christianrodriguezt@gmail.com>
2019-05-23 17:26:11 -0500

    Creacion de rutas y Contexto

--- index.js
+++ index.js
@@ -11,10 +11,50 @@ app.engine('handlebars',motorRender());
 app.set('view engine','handlebars');
 
 
-app.get('/', function(req, res) {
-    res.send('<h1>Hola Mundo!</h1>');
-    
+//Ruta de inicio
+app.get('/inicio', function(req, res) {
+
+    res.render('main', contexto[0]);
+
  });
+
+ //Ruta de Sobre mi
+app.get('/sobre', function(req, res) {
+
+    res.render('main', contexto[1]);
+
+ });
+
+ //Ruta de Contacto
+app.get('/contacto', function(req, res) {
+
+    res.render('main', contexto[2]);
+
+ });
+
+
+
+var contexto = [
+    {
+        titulo: 'Inicio',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        layaout: false,
+    },
+
+    {
+        titulo: 'Sobre mi',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        layaout: false,
+    },
+
+    {
+        titulo: 'Contacto',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        layaout: false,
+    }
+]
+
+
 
 //Servidor creado
 app.listen(3000, function() {
