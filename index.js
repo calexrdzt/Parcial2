9e5ee81dbcbefef56df46f0292e2631f43344dae
Christian Rodriguez <christianrodriguezt@gmail.com>
2019-05-23 17:07:32 -0500

    Se creo lo necesario para el servidor

---
+++ index.js
@@ -0,0 +1,21 @@
+//Christian Alexander Rodriguez Timarán - A00010104
+
+
+var express = require('express');
+var app = express();
+const fs= require('fs');
+var motorRender= require('express-handlebars');
+app.use(express.static('public'));
+app.engine('handlebars',motorRender());
+app.set('view engine','handlebars');
+
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hola Mundo!</h1>');
+    
+ });
+
+//Servidor creado
+app.listen(3000, function() {
+    console.log('Entro perro');
+});
