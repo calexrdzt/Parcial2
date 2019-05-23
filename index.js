8403959dabad7b955cdb2bedc57ce034b2f9034d
Christian Rodriguez <christianrodriguezt@gmail.com>
2019-05-23 17:33:13 -0500

    Ahora isi ya funcionan las rutas xD

--- index.js
+++ index.js
@@ -11,26 +11,7 @@ app.engine('handlebars',motorRender());
 app.set('view engine','handlebars');
 
 
-//Ruta de inicio
-app.get('/inicio', function(req, res) {
 
-    res.render('main', contexto[0]);
-
- });
-
- //Ruta de Sobre mi
-app.get('/sobre', function(req, res) {
-
-    res.render('main', contexto[1]);
-
- });
-
- //Ruta de Contacto
-app.get('/contacto', function(req, res) {
-
-    res.render('main', contexto[2]);
-
- });
 
 
 
@@ -38,22 +19,41 @@ var contexto = [
     {
         titulo: 'Inicio',
         des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        layaout: false,
+        layout: false,
     },
 
     {
         titulo: 'Sobre mi',
         des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        layaout: false,
+        layout: false,
     },
 
     {
         titulo: 'Contacto',
         des: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis eu eros nec pulvinar. Suspendisse faucibus mauris eget turpis aliquet faucibus a congue ante. Donec tincidunt placerat justo, et iaculis urna tempus id. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-        layaout: false,
+        layout: false,
     }
 ]
+//Ruta de inicio
+app.get('/inicio', function(req, res) {
 
+    res.render('plantilla', contexto[0]);
+
+ });
+
+ //Ruta de Sobre mi
+app.get('/sobre', function(req, res) {
+
+    res.render('plantilla', contexto[1]);
+
+ });
+
+ //Ruta de Contacto
+app.get('/contacto', function(req, res) {
+
+    res.render('plantilla', contexto[2]);
+
+ });
 
 
 //Servidor creado
